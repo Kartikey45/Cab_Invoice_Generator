ee0ec635b6fcd3f60f38db4ef3e751328c7f8a0c
Kartikey45 <singhkartikey45@gmail.com>
2020-05-10 00:43:18 +0530

    ADDED : Step 3 Enhanced Invoice

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -4,6 +4,9 @@ namespace CabInvoiceGenerator
 {
     public class InvoiceGenerator
     {
+        //variable initialize
+        int numberOfRides = 5;
+
         //Calculate total fair of a single journey
         public int totalFair(int distanceInKm, int timeInMinute )
         {
@@ -18,11 +21,11 @@ namespace CabInvoiceGenerator
         }
 
         //Calculate the aggregate total fair of multiple rides
-        public int aggregateFare(int numberOfRides)
+        public int aggregateFare()
         {
             //variable initialize
             int aggregateFare = 0;
-
+            
             //create instance variable
             InvoiceGenerator cabInvoice = new InvoiceGenerator();
             
@@ -32,6 +35,24 @@ namespace CabInvoiceGenerator
                 aggregateFare = aggregateFare + cabInvoice.totalFair(5, 60);
             }
             return aggregateFare;
+        }
+
+        //Total nuber of rides
+        public int totalNumberOfRides()
+        {
+            return numberOfRides;
+        }
+
+        //Average fare per ride
+        public int averagFarePerRide()
+        {
+            //create instance variable 
+            InvoiceGenerator cabInvoice = new InvoiceGenerator();
+            
+            //calculate average fare
+            int average = (cabInvoice.aggregateFare() / totalNumberOfRides());
+            
+            return average;
         }
     }
 }
--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -19,8 +19,26 @@ namespace CabInvoiceGeneratorTest
         public void agregatefare()
         {
             InvoiceGenerator generate = new InvoiceGenerator();
-            int aggregate = generate.aggregateFare(5);
+            int aggregate = generate.aggregateFare();
             Assert.AreEqual(aggregate,575);
+        }
+        
+        //Total number of rides
+        [Test]
+        public void totalRides()
+        {
+            InvoiceGenerator generate = new InvoiceGenerator();
+            int totalrides = generate.totalNumberOfRides();
+            Assert.AreEqual(5, totalrides);
+        }
+
+        //Average fare per ride
+        [Test]
+        public void AverageFarePerRide()
+        {
+            InvoiceGenerator generate = new InvoiceGenerator();
+            int averageFare = generate.averagFarePerRide();
+            Assert.AreEqual(115, averageFare);
         }
     }
 }
