97b5f23e49123c487c7ebca6e4fc49fe399a17ff
Kartikey45 <singhkartikey45@gmail.com>
2020-05-09 21:22:14 +0530

    ADDED : Welcome to Cab Invoice Generator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Print welcome message
+            Console.WriteLine("Welcome to cab invoice generator");
+        }
+    }
+}
